8019faf23e7a0dba234ab0eba7270518bb45366e
Saman Seifi <samansei@bu.edu>
2024-04-07 00:44:08 -0400

    changing vectors and matrices and add solver

--- src/main.cpp
+++ src/main.cpp
@@ -1,182 +1,227 @@
 /* Simulating a simple linear beam */
+#include <iostream>
+#include <vector>
+#include <cassert>
 
-#include "Mesh.h"
+extern "C" {
+    void dgesv_(int* n, int* nrhs, double* a, int* lda, int* ipiv, double* b, int* ldb, int* info);
+}
 
-using namespace std;
 
-/* Assigning the weighting for numerical integration */
-void GetWeights(vector<double> &xi, vector<double> &w, int numIP);
+struct Matrix {
+    std::vector<double> data;
+    int rows, cols;
+
+    Matrix(int m, int n) : data(m * n), rows(m), cols(n) {}
+
+    double& operator()(int i, int j) {
+        return data[i * cols + j];
+    }
+
+    void removeRow(int rowToRemove) {
+        assert(rowToRemove < rows);
+        data.erase(data.begin() + rowToRemove * cols, data.begin() + (rowToRemove + 1) * cols);
+        --rows;
+    }
+
+    void removeCol(int colToRemove) {
+        assert(colToRemove < cols);
+        for (int i = rows - 1; i >= 0; --i) {
+            data.erase(data.begin() + i * cols + colToRemove);
+        }
+        --cols;
+    }
+};
+
+struct Vector {
+    std::vector<double> data;
+
+    explicit Vector(int n) : data(n) {}
+
+    double& operator[](int i) {
+        return data[i];
+    }
+
+    void remove(int indexToRemove) {
+        assert(indexToRemove >= 0 && static_cast<size_t>(indexToRemove) < data.size());
+        data.erase(data.begin() + indexToRemove);
+    }
+
+
+    int size() const {
+        return data.size();
+    }
+};
+
+void solveLinearSystem(Matrix& A, Vector& b) {
+    assert(A.rows == A.cols && b.size() == A.rows); // Ensure system is consistent
+    int n = A.rows, nrhs = 1, lda = n, ldb = n, info;
+    std::vector<int> ipiv(n);
+
+    dgesv_(&n, &nrhs, &A.data[0], &lda, &ipiv[0], &b.data[0], &ldb, &info);
+
+    if (info > 0) {
+        std::cerr << "Solution could not be computed; matrix is singular.\n";
+    } else if (info < 0) {
+        std::cerr << "Argument " << -info << " had an illegal value.\n";
+    }
+}
+
 
 
 
 int main() {
 
-    /** \name Geometry: Area simple bat with a given beam_length and cross section area = Area  */
-    const double beam_length = 5.0;
-    const double Area = 1.0;
+    // Beam length 10m
+    // Number of elements 10
+    // Number of nodes 11
+    // Number of DOFs 22
+    double L_beam = 10.0;
+    int n_elements = 2;
+    int n_nodes = n_elements + 1;
+    int n_dofs = 3 * n_nodes;
 
-    /** \name Material properties
-     *          mu: Shear modulus
-     *          nu: Poisson Ratio
-     **/
-    /*@{*/
-    const double mu = 50.0;
-    const double nu = 0.3;
-    const double cnst = 2.0 * mu * Area * (1 - nu) / (1 - 2 * nu);
-    /*@}*/
+    // create the nodal coordinates
+    Matrix coordinates(n_nodes, 2);
+    for (int i = 0; i < n_nodes; i++) {
+        coordinates(i, 0) = i * L_beam / n_elements;
+        coordinates(i, 1) = 0.0;
+    }
 
-    /** \name Defining Loadings */
-    /*@{*/
-    const double bodyforce = 10.0;
-    const double traction = 2.0;
-    /*@}*/
-
-    /** Total # of elements (nel) and # of nodes per element (2 for linear, 3 for quadratic elements) */
-    /*@{*/
-    int nel = 10;
-    int nnodes_per_el = 3;
-    /*@}*/
-
-    Mesh mesh_beam(beam_length, nel, nnodes_per_el);
-
-    /** Setting up the data structures for the mesh */
-    /*@{*/
-    /* 1) Building Vector of nodal coordinates */
-    VectorT<double> vecCoords = mesh_beam.GetCoords();
+    // Mesh connectivity
+    int connectivity[n_elements][2];
+    for (int i = 0; i < n_elements; i++) {
+        connectivity[i][0] = i;
+        connectivity[i][1] = i + 1;
+    }
 
 
-    /* 2) Building matrix of element connectivity (specifies node numbers on each element) */
-    MatrixT<int> matConnect = mesh_beam.GetConnectivity();
-    /*@{*/
+    
+    double E = 210; // Young's modulus
+    double A = 0.01; // Cross-sectional area
+    double L = 1.0; // Length of the beam element
+    double I = 0.0001; // Moment of inertia
 
-    int nnodes = mesh_beam.GetNumNodes();
+    double C1 = E * A / L;
+    double C2 = E * I / L*L*L;
 
-    /* Integration points and weights for 2 point integration */
-    /*@{*/
-    int numIP = nnodes_per_el - 1;
-    vector<double> xi(2), w(2);
-    GetWeights(xi, w, numIP);
-    /*@{*/
+    // Create element stiffness
+    Matrix elementStiffness(6, 6);
 
-    /* Assemble the global stiffness and force vector */
-    MatrixT<double> K(nnodes);
-    VectorT<double> F(nnodes);
+    elementStiffness(0, 0) = C1;
+    elementStiffness(0, 1) = 0.0;
+    elementStiffness(0, 2) = 0.0;
+    elementStiffness(0, 3) = -C1;
+    elementStiffness(0, 4) = 0.0;
+    elementStiffness(0, 5) = 0.0;
+    
+    elementStiffness(1, 0) = 0.0;
+    elementStiffness(1, 1) = 12 * C2;
+    elementStiffness(1, 2) = 6 * C2 * L;
+    elementStiffness(1, 3) = 0.0;
+    elementStiffness(1, 4) = -12 * C2;
+    elementStiffness(1, 5) = 6 * C2 * L;
 
-    /* Loop over elements! */
-    for (int elem = 0; elem < nel; elem++) {
+    elementStiffness(2, 0) = 0.0;
+    elementStiffness(2, 1) = 6 * C2 * L;
+    elementStiffness(2, 2) = 4 * C2 * L*L;
+    elementStiffness(2, 3) = 0.0;
+    elementStiffness(2, 4) = -6 * C2 * L;
+    elementStiffness(2, 5) = 2 * C2 * L*L;
 
-        /* Extract the vecCoords of each node on the current element */
-        MatrixT<double> K_el;
-        K_el.Dimension(nnodes_per_el, nnodes_per_el);
+    elementStiffness(3, 0) = -C1;
+    elementStiffness(3, 1) = 0.0;
+    elementStiffness(3, 2) = 0.0;
+    elementStiffness(3, 3) = C1;
+    elementStiffness(3, 4) = 0.0;
+    elementStiffness(3, 5) = 0.0;
 
-        VectorT<double> F_el;
-        F_el.Dimension(nnodes_per_el);
+    elementStiffness(4, 0) = 0.0;
+    elementStiffness(4, 1) = -12 * C2;
+    elementStiffness(4, 2) = -6 * C2 * L;
+    elementStiffness(4, 3) = 0.0;
+    elementStiffness(4, 4) = 12 * C2;
+    elementStiffness(4, 5) = -6 * C2 * L;
 
-        /* Extract nodal coordinates of each element */
-        VectorT<double> vecElemCoords;
-        vecElemCoords.Dimension(nnodes_per_el);
+    elementStiffness(5, 0) = 0.0;
+    elementStiffness(5, 1) = 6 * C2 * L;
+    elementStiffness(5, 2) = 2 * C2 * L*L;
+    elementStiffness(5, 3) = 0.0;
+    elementStiffness(5, 4) = -6 * C2 * L;
+    elementStiffness(5, 5) = 4 * C2 * L*L;
 
-        for (int a = 0; a < nnodes_per_el; a++) {
-            vecElemCoords[a] = vecCoords[matConnect(a, elem)];
-        }
+    // construct the global stiffness matrix
+    Matrix globalStiffness(n_dofs, n_dofs);
 
-        /* Loop over number of integration points for the current element and assemble element stiffness!  */
-        for (int II = 0; II < numIP; II++) {
+    for (int i = 0; i < n_elements; i++) {
+        int node1 = connectivity[i][0];
+        int node2 = connectivity[i][1];
 
-            /* Compute shape functions and its derivatives: N and dN/dxi at the current integration point */
-            VectorT<double> N;
-            N.Dimension(nnodes_per_el);
+        int dof1 = 3 * node1;
+        int dof2 = 3 * node2;
 
-            VectorT<double> dNdxi;
-            dNdxi.Dimension(nnodes_per_el);
-
-            /* Element order = number of nodes - 1 */
-            if (nnodes_per_el == 3) {
-                N[0] = -0.5 * xi[II] * (1.0 - xi[II]);
-                N[1] =  0.5 * xi[II] * (1.0 + xi[II]);
-                N[2] =  1.0 - pow(xi[II], 2);
-                dNdxi[0] = -0.5 + xi[II];
-                dNdxi[1] =  0.5 + xi[II];
-                dNdxi[2] = -2.0 * xi[II];
-            } else if (nnodes_per_el == 2) {
-                N[0] = 0.5 * (1.0 - xi[II]);
-                N[1] = 0.5 * (1.0 + xi[II]);
-                dNdxi[0] = -0.5;
-                dNdxi[1] =  0.5;
-            }
-
-            /* Compute dx/dxi, J and dN/dx */
-            double dxdxi = 0.0;
-            for (int a = 0; a < nnodes_per_el; a++) {
-                dxdxi = dxdxi + dNdxi[a] * vecElemCoords[a];
-            }
-            /* Calculate the Jacobian */
-            double J = abs(dxdxi);
-
-            VectorT<double> dNdx;
-            dNdx.Dimension(nnodes_per_el);
-            for (int a = 0; a < nnodes_per_el; a++) {
-                dNdx[a] = dNdxi[a] / dxdxi;
-            }
-
-            /* Add contribution to element stiffness and force vector from current integration pt */
-            for (int a = 0; a < nnodes_per_el; a++) {
-                F_el[a] += w[II] * bodyforce * J * N[a];
-                for (int b = 0; b < nnodes_per_el; b++) {
-                    K_el(a, b) += cnst * w[II] * J * dNdx[a] * dNdx[b];
+        for (int j = 0; j < 6; j++) {
+            for (int k = 0; k < 6; k++) {
+                if (j < 3 && k < 3) {
+                    globalStiffness(dof1 + j % 3, dof1 + k % 3) += elementStiffness(j, k);
+                } else if (j < 3) {
+                    globalStiffness(dof1 + j % 3, dof2 + k % 3) += elementStiffness(j, k);
+                } else if (k < 3) {
+                    globalStiffness(dof2 + j % 3, dof1 + k % 3) += elementStiffness(j, k);
+                } else {
+                    globalStiffness(dof2 + j % 3, dof2 + k % 3) += elementStiffness(j, k);
                 }
             }
         }
-
-        /* Add the stiffness and residual from the current element into global matrices */
-        for (int a = 0; a < nnodes_per_el; a++) {
-            int rw = matConnect(a, elem);
-            F[rw] += F_el[a];
-            for (int b = 0; b < nnodes_per_el; b++) {
-                int cl = matConnect(b, elem);
-                K(rw, cl) += K_el(a, b);
-            }
-        }
     }
 
-    /*  Add the extra forcing term from the traction at x=nel */
-    //TODO: It should be generalized to a set of nodes.
-    F[nnodes-1] = F[nnodes-1] + traction;
-
-    /**
-     * Modify FEM equations to enforce displacement boundary condition
-     * To do this we simply replace the equation for the first node with u=0
-     **/
-     //TODO: This also should be generalized to a set of nodes.
-    for (int a = 0; a < nnodes; a++)
-        K(0, a) = 0.0;
-
-    // K(0,0) = 1 means when it is multiplied by the
-    K(0, 0) = 1.0;
-    F[0] = 0.0;
-    K.Print();
 
 
 
-    /* Solving the linear system */
+    // create the force vector
+    Vector force(n_dofs);
+
+    // add vertical load at final node
+    force[3 * n_nodes - 2] = -1.0;
+
+    // remove corresponding force elements to known displacements
+    Vector updatedForces = force;
+    updatedForces.remove(0);
+    updatedForces.remove(0);
+    updatedForces.remove(0);
+
+    
+
+    Matrix updatedStiffness = globalStiffness;
+    // Remove cols 0 1 and 2 use removeCol method which is one col at a time
+    updatedStiffness.removeCol(0);
+    updatedStiffness.removeRow(0);
+    updatedStiffness.removeCol(0);
+    updatedStiffness.removeRow(0);
+    updatedStiffness.removeCol(0);
+    updatedStiffness.removeRow(0);
 
 
-    //SolveLinear(sol, K, F);
+
+    // globalStiffness.print();
+    // std::cout << "A=" << std::endl;
+    // updatedStiffness.print();
+    // // force.print();
+    // std::cout << "b=" << std::endl;
+    // updatedForces.print();
+
+    solveLinearSystem(updatedStiffness, updatedForces);
+
+    std::cout << "Solution of the modified system:\n";
+    for (int i = 0; i < updatedForces.size(); ++i) {
+        std::cout << "x[" << i << "] = " << updatedForces[i] << std::endl;
+    }
+
+
+
+
 
     return 0;
-}
-
-/* Getting the weights for numerical integration */
-void GetWeights(vector<double> &xi, vector<double> &w, const int numIP) {
-    if (numIP == 2) {
-        w[0] = w[1] = 1.0;
-        xi[0] = -0.5773502692;
-        xi[1] =  0.5773502692;
-    } else if (numIP == 1) {
-        w[0] = 2.0;
-        w[1] = 0.0;
-        xi[0] = xi[1] = 0.0;
-    }
 }
 
 
